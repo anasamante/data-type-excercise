ae96911e74148aa982a7f09da96911c9fc0e890e
Ana Amante <ana.amante@overactiveinc.com>
2019-12-19 17:46:07 -0300

    first commit.

---
+++ src/test/java/DataTypeExample.java
@@ -0,0 +1,36 @@
+public class DataTypeExample {
+    /**
+     comentario
+     **/
+    public void displayCarInfo(String brand, String chasisNum){
+
+        System.out.println("");
+
+    }
+
+    public static void main(String[] args) {
+
+        /* car attributes*/
+        /*primitives*/
+
+        int qtyWheels = 0;
+        int qtyLights = 0:
+        int doors = 0;
+
+        char prefixDomain = 'A';
+
+        boolean turnOn = false;
+
+        /*non primitives*/
+
+        String brand = 'Ford';
+        String fullDomain = null;
+
+        /*verificar*/
+        final String CHASIS_NUM = 'ASDASD';
+
+
+
+
+    }
+}
