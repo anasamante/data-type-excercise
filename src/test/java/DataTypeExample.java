712bbf04b223d1587c3ba355655a0b369dd0bff4
Ana Amante <ana.amante@overactiveinc.com>
2019-12-26 17:42:31 -0300

    new array

--- src/test/java/DataTypeExample.java
+++ src/test/java/DataTypeExample.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class DataTypeExample {
     /**
      * comentario
@@ -5,14 +7,21 @@ public class DataTypeExample {
 
     public static void main(String[] args) {
 
-        Car car = new Car(4, 2, 4, "b", false, "abc", "asdffas12343");
+        Car carOne = new Car(4, 2, 4, "b", false, "abc", "asdffas12343");
+        Car carTwo = new Car(4, 2, 4, "b", false, "abc", "asdffas12343");
+        Car carThree = new Car(4, 2, 4, "b", false, "abc", "asdffas12343");
 
-        car.setDoors(5);
-        car.setBrand("FIAT");
+        carOne.setDoors(5);
+        carOne.setBrand("FIAT");
 
-        car.getQtyLights();
-        car.getDoors();
-        car.getBrand();
+        carOne.getQtyLights();
+        carOne.getDoors();
+        carOne.getBrand();
+
+        ArrayList<Car> carArrayList = new ArrayList<Car>();
+
+        carArrayList.add(carOne);
+        carArrayList.add(carTwo);
 
 
     }
